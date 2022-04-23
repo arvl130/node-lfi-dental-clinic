b2cfe2d524bb1643c22ee50064cade32be5c8755
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-23 17:49:08 +0800

    messages.js: replace callbacks with files from controllers folder

--- routes/messages.js
+++ routes/messages.js
@@ -1,38 +1,11 @@
 const express = require("express");
-const getAllMessages = require("../controllers/getAllMessages");
-const deleteMessage = require("../controllers/deleteMessage");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const messages = await getAllMessages();
+const getAllMessages = require("../controllers/messages/getAllMessages");
+const deleteMessage = require("../controllers/messages/deleteMessage");
 
-  res.status(200).json({
-    message: "List of all messages",
-    payload: messages,
-  });
-});
+router.get("/", getAllMessages);
 
-router.get("/:uid/delete", async (req, res) => {
-  try {
-    const uid = req.params.uid;
-
-    if (!uid) {
-      const error = new Error("Missing or invalid UID");
-      error.httpErrorCode = 400;
-      throw error;
-    }
-
-    const message = deleteMessage(uid);
-
-    res.status(200).json({
-      message: "Message deleted",
-      payload: message,
-    });
-  } catch (e) {
-    res.status(e.httpErrorCode || 500).json({
-      message: e.message || "Error occured while deleting message",
-    });
-  }
-});
+router.get("/:uid/delete", deleteMessage);
 
 module.exports = router;
