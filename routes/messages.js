cbd7ea8a617e055e2c203b7d80b3ce0bacbb0487
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-23 18:20:52 +0800

    messages.js: add route for creating new messages

--- routes/messages.js
+++ routes/messages.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const getAllMessages = require("../controllers/messages/getAllMessages");
 const deleteMessage = require("../controllers/messages/deleteMessage");
+const newMessage = require("../controllers/messages/newMessage");
 
 router.get("/", getAllMessages);
-
+router.put("/", newMessage);
 router.delete("/:uid/delete", deleteMessage);
 
 module.exports = router;
