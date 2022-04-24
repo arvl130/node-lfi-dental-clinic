b579339775ffc6c048bf69302487307d6bdfa367
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-24 14:10:04 +0800

    routes/auth.js: fix import path to register admin user controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const registerAdminUser = require("../helpers/registerAdminUser");
+const registerAdminUser = require("../controllers/auth/registerAdminUser");
 
 router.post("/register/admin", registerAdminUser);
 
