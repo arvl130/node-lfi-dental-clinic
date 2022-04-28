313477eba2206fbf27cea4fa778b066e1d680c93
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-28 23:23:26 +0800

    routes/schedules.js: add routes to add and delete closed slot

--- routes/schedules.js
+++ routes/schedules.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const addClosedSlot = require("../controllers/schedules/addClosedSlot");
+const deleteClosedSlot = require("../controllers/schedules/deleteClosedSlot");
+const getClosedSlots = require("../controllers/schedules/getClosedSlots");
 const router = express.Router();
 
-const getClosedSlots = require("../controllers/schedules/getClosedSlots");
-
 router.get("/closed/:startSeconds/:endSeconds", getClosedSlots);
+router.put("/closed/", addClosedSlot);
+router.delete("/closed/:slotSeconds", deleteClosedSlot);
 
 module.exports = router;
