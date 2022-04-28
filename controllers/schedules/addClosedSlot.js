c3ca85b6f931c5e6afdf3b2b2b1a2131dc1e0a6c
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-28 23:22:47 +0800

    controllers/addClosedSlot.js: add controller

---
+++ controllers/schedules/addClosedSlot.js
@@ -0,0 +1,25 @@
+const addClosedSlot = require("../../helpers/calendar/addClosedSlot");
+const HttpError = require("../../helpers/HttpError");
+
+module.exports = async (req, res) => {
+  try {
+    const slotSeconds = req.body.slotSeconds;
+
+    if (!slotSeconds)
+      throw new HttpError(
+        `Missing or invalid slot seconds: ${slotSeconds}`,
+        400
+      );
+
+    await addClosedSlot(slotSeconds);
+
+    res.status(200).json({
+      message: "Closed slot added",
+      payload: slotSeconds,
+    });
+  } catch (e) {
+    res.status(e.httpErrorCode || 500).json({
+      message: e.message || "Error occured while adding closed dates",
+    });
+  }
+};
