db686662d0b60d15103a2c80a8bd0f33b6b8ac03
Angelo Geulin <angelogeulin123@gmail.com>
2022-04-28 23:22:28 +0800

    helpers/deleteClosedSlot.js: add helper

---
+++ helpers/calendar/deleteClosedSlot.js
@@ -0,0 +1,6 @@
+const { getFirestore } = require("firebase-admin/firestore");
+const db = getFirestore();
+
+module.exports = async (slotSeconds) => {
+  await db.collection("closed_dates").doc(slotSeconds.toString()).delete();
+};
