b1053e365d767c61b296b743d7fd113436bbaae4
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-03 15:55:09 +0800

    helpers/createSlotRecord.js: add helper

---
+++ helpers/timeslots/createSlotRecord.js
@@ -0,0 +1,6 @@
+const { getFirestore } = require("firebase-admin/firestore");
+const db = getFirestore();
+
+module.exports = async (slotSeconds, payload) => {
+  await db.collection("timeslots").doc(slotSeconds.toString()).set(payload);
+};
