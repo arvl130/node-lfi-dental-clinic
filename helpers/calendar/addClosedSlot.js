3cd2f3e756368210ea8005fdbe8958dba7952581
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-03 15:57:45 +0800

    helpers/addClosedSlot.js, helpers/deleteClosedSlot.js: delete helpers

--- helpers/calendar/addClosedSlot.js
+++
@@ -1,8 +0,0 @@
-const { getFirestore } = require("firebase-admin/firestore");
-const db = getFirestore();
-
-module.exports = async (slotSeconds) => {
-  await db.collection("timeslots").doc(slotSeconds.toString()).set({
-    status: "closed",
-  });
-};
--- helpers/calendar/deleteClosedSlot.js
+++
@@ -1,6 +0,0 @@
-const { getFirestore } = require("firebase-admin/firestore");
-const db = getFirestore();
-
-module.exports = async (slotSeconds) => {
-  await db.collection("timeslots").doc(slotSeconds.toString()).delete();
-};
