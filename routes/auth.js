d4de48d03a601c13085558f48d882de988ef209c
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-06 03:01:10 +0800

    routes/timeslots.js: register route for registering patient users

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const registerAdminUser = require("../controllers/auth/registerAdminUser");
+const registerPatientUser = require("../controllers/auth/registerPatientUser");
 
 router.post("/register/admin", registerAdminUser);
+router.post("/register", registerPatientUser);
 
 module.exports = router;
