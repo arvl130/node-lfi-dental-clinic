104a594a28b4b2cb76922daa57a802f43ced9246
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-06 03:03:33 +0800

    helpers/newAppointment.js: add helper

---
+++ helpers/appointments/newAppointment.js
@@ -0,0 +1,40 @@
+const { getFirestore, FieldValue } = require("firebase-admin/firestore");
+const getMonthSecondsFromSlotSeconds = require("../conversions/getMonthSecondsFromSlotSeconds");
+const db = getFirestore();
+
+module.exports = async (patientUid, slotSeconds, service) => {
+  const monthSeconds = getMonthSecondsFromSlotSeconds(parseInt(slotSeconds));
+  const batch = db.batch();
+  const userAppointmentsRef = db
+    .collection("users")
+    .doc(patientUid)
+    .collection("appointments")
+    .doc(slotSeconds.toString());
+
+  const reservationsRef = db
+    .collection("monthlyReservations")
+    .doc(monthSeconds.toString());
+
+  batch.set(userAppointmentsRef, {
+    createdAt: FieldValue.serverTimestamp(),
+    service,
+    procedure: "",
+    price: null,
+    balance: null,
+    status: null,
+  });
+
+  batch.update(
+    reservationsRef,
+    {
+      [slotSeconds]: {
+        status: "taken",
+      },
+    },
+    {
+      merge: true,
+    }
+  );
+
+  await batch.commit();
+};
