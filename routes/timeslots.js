985544008e553ef623dda64a312121031b5e964d
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-06 03:06:26 +0800

    routes/timeslots.js: register middleware

--- routes/timeslots.js
+++ routes/timeslots.js
@@ -5,6 +5,7 @@ const addClosedSlot = require("../controllers/schedules/addClosedSlot");
 const deleteClosedSlot = require("../controllers/schedules/deleteClosedSlot");
 const getClosedSlots = require("../controllers/schedules/getClosedSlots");
 const getUnavailableSlots = require("../controllers/timeslots/getUnavailableSlots");
+const requirePatientToken = require("../middleware/requirePatientToken");
 const router = express.Router();
 
 /* Admin only */
@@ -13,7 +14,7 @@ router.delete("/closed/:slotSeconds", deleteClosedSlot);
 router.get("/closed/:year/:month", getClosedSlots); // deprecated
 
 /* Anyone */
-router.put("/appointments", newAppointment);
+router.put("/appointments", requirePatientToken, newAppointment);
 router.patch("/appointments/:slotSeconds", cancelAppointment); // patients only
 
 router.get("/unavailable/:year/:month", getUnavailableSlots);
