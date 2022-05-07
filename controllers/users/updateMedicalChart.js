15c99af7e72e312efc01ca268c61c6aa96fad948
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-07 13:15:04 +0800

    controllers/updateMedicalChart.js: add controller for updating medical chart

---
+++ controllers/users/updateMedicalChart.js
@@ -0,0 +1,47 @@
+const { getAuth } = require("firebase-admin/auth");
+const auth = getAuth();
+const HttpError = require("../../helpers/HttpError");
+const updateMedicalChart = require("../../helpers/users/updateMedicalChart");
+
+module.exports = async (req, res) => {
+  try {
+    const personalInformation = req.body.personalInformation;
+    const medicalHistory = req.body.medicalHistory;
+    const dentalHistory = req.body.dentalHistory;
+    const patientUid = req.patientUid;
+
+    if (!personalInformation)
+      throw new HttpError("Missing or invalid personal information", 400);
+
+    if (!medicalHistory)
+      throw new HttpError("Missing or invalid medical history", 400);
+
+    if (!dentalHistory)
+      throw new HttpError("Missing or invalid dental history", 400);
+
+    await auth.updateUser(patientUid, {
+      displayName: personalInformation.fullName,
+    });
+
+    await updateMedicalChart(
+      patientUid,
+      personalInformation,
+      medicalHistory,
+      dentalHistory
+    );
+
+    res.status(200).json({
+      message: "Updated medical chart",
+      payload: {
+        uid: patientUid,
+        personalInformation,
+        medicalHistory,
+        dentalHistory,
+      },
+    });
+  } catch (e) {
+    res.status(e.httpErrorCode || 500).json({
+      message: `Error occured while updating medical chart: ${e.message}`,
+    });
+  }
+};
