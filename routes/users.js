68b52ec008f329f950f8f75836181b9913069f82
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-07 21:06:49 +0800

    users.js: register routes needed by patient account

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,36 @@ router.patch(
   updateMedicalChart
 );
 
+router.get(
+  "/:patientUid/medicalchart",
+  requirePatientToken,
+  updateMedicalChart
+);
+
+router.patch(
+  "/:patientUid/dentalchart",
+  requirePatientToken,
+  updateMedicalChart
+);
+
+router.get("/:patientUid/dentalchart", requirePatientToken, updateMedicalChart);
+
+router.get(
+  "/:patientUid/appointments",
+  requirePatientToken,
+  updateMedicalChart
+);
+
+router.delete(
+  "/:patientUid/appointments/:slotSeconds/procedure",
+  requirePatientToken,
+  updateMedicalChart
+);
+
+router.delete(
+  "/:patientUid/appointments/:slotSeconds/cancel",
+  requirePatientToken,
+  updateMedicalChart
+);
+
 module.exports = router;
