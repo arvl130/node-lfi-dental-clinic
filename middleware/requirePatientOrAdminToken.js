79627aac9c589757247872ccd6b9f9a3b6a65c05
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-09 00:21:18 +0800

    requirePatientOrAdminToken.js: add middleware

---
+++ middleware/requirePatientOrAdminToken.js
@@ -0,0 +1,33 @@
+const { getAuth } = require("firebase-admin/auth");
+const HttpError = require("../helpers/HttpError");
+
+const auth = getAuth();
+
+module.exports = async (req, res, next) => {
+  try {
+    const authorizationHeader = req.headers.authorization;
+
+    if (!authorizationHeader)
+      throw new HttpError("Missing or invalid authorization header", 400);
+
+    const idToken = authorizationHeader.split(" ")[1];
+
+    if (!idToken) throw new HttpError("Missing or invalid ID token", 400);
+
+    const decodedToken = await auth.verifyIdToken(idToken);
+
+    if (
+      decodedToken.accountType === "patient" ||
+      decodedToken.accountType === "admin"
+    ) {
+      req.patientUid = decodedToken.uid;
+      next();
+    } else {
+      throw new HttpError("Unauthorized request", 401);
+    }
+  } catch (e) {
+    res.status(e.httpErrorCode || 500).json({
+      message: `Error occured while getting verifying token: ${e.message}`,
+    });
+  }
+};
