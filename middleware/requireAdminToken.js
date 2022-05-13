8f217f92164dc6ccacf8350ffccd1852d250d909
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 12:09:03 +0800

    requireAdminToken.js: add middleware

---
+++ middleware/requireAdminToken.js
@@ -0,0 +1,28 @@
+const { getAuth } = require("firebase-admin/auth");
+const HttpError = require("../helpers/HttpError");
+
+const auth = getAuth();
+
+module.exports = async (req, res, next) => {
+  try {
+    const authorizationHeader = req.headers.authorization;
+
+    if (!authorizationHeader)
+      throw new HttpError("Missing or invalid authorization header", 400);
+
+    const idToken = authorizationHeader.split(" ")[1];
+
+    if (!idToken) throw new HttpError("Missing or invalid ID token", 400);
+
+    const decodedToken = await auth.verifyIdToken(idToken);
+
+    if (!decodedToken.accountType === "admin")
+      throw new HttpError("Unauthorized request", 401);
+
+    next();
+  } catch (e) {
+    res.status(e.httpErrorCode || 500).json({
+      message: `Error occured while getting verifying token: ${e.message}`,
+    });
+  }
+};
