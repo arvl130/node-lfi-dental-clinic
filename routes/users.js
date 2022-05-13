8854e77a97303dc285cbbf2848bd15fa155b772f
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-13 12:24:54 +0800

    users.js: register routes for users appointments, attended status, procedure, and medical chart

--- routes/users.js
+++ routes/users.js
@@ -3,21 +3,61 @@ const router = express.Router();
 
 const updateMedicalChart = require("../controllers/users/updateMedicalChart");
 const getUserProfile = require("../controllers/users/getUserProfile");
+const listUsers = require("../controllers/users/listUsers");
 const requirePatientToken = require("../middleware/requirePatientToken");
 const requirePatientOrAdminToken = require("../middleware/requirePatientOrAdminToken");
+const requireAdminToken = require("../middleware/requireAdminToken");
+const getUserAppointments = require("../controllers/users/getUserAppointments");
+const setUserAppointmentAttended = require("../controllers/users/setUserAppointmentAttended");
+const unsetUserAppointmentAttended = require("../controllers/users/unsetUserAppointmentAttended");
+const getUserAppointmentProcedure = require("../controllers/users/getUserAppointmentProcedure");
+const setUserAppointmentProcedure = require("../controllers/users/setUserAppointmentProcedure");
+const getMedicalChart = require("../controllers/users/medical-chart/getMedicalChart");
+
+router.get("/", requireAdminToken, listUsers);
 
 router.get("/:patientUid", requirePatientOrAdminToken, getUserProfile);
 
+router.get(
+  "/:patientUid/appointments",
+  requirePatientOrAdminToken,
+  getUserAppointments
+);
+
+router.patch(
+  "/:patientUid/appointments/:slotSeconds/attended",
+  requireAdminToken,
+  setUserAppointmentAttended
+);
+
+router.delete(
+  "/:patientUid/appointments/:slotSeconds/attended",
+  requireAdminToken,
+  unsetUserAppointmentAttended
+);
+
+router.get(
+  "/:patientUid/appointments/:slotSeconds/procedure",
+  requireAdminToken,
+  getUserAppointmentProcedure
+);
+
+router.patch(
+  "/:patientUid/appointments/:slotSeconds/procedure",
+  requireAdminToken,
+  setUserAppointmentProcedure
+);
+
 router.patch(
   "/:patientUid/medicalchart",
-  requirePatientToken,
+  requirePatientOrAdminToken,
   updateMedicalChart
 );
 
 router.get(
   "/:patientUid/medicalchart",
-  requirePatientToken,
-  updateMedicalChart
+  requirePatientOrAdminToken,
+  getMedicalChart
 );
 
 router.patch(
@@ -27,12 +67,6 @@ router.patch(
 );
 
 router.get("/:patientUid/dentalchart", requirePatientToken, updateMedicalChart);
-
-router.get(
-  "/:patientUid/appointments",
-  requirePatientToken,
-  updateMedicalChart
-);
 
 router.delete(
   "/:patientUid/appointments/:slotSeconds/procedure",
