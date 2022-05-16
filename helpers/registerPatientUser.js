194f3ea86cf9235e2949d00e94a441c6a41d8c65
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-17 02:12:07 +0800

    helpers/registerPatientUser.js: throw HTTP error when email already exists

--- helpers/registerPatientUser.js
+++ helpers/registerPatientUser.js
@@ -5,30 +5,36 @@ const auth = getAuth();
 const HttpError = require("./HttpError");
 
 module.exports = async (email, password, fullName) => {
-  const userRecord = await auth.createUser({
-    displayName: fullName,
-    email,
-    password,
-  });
+  try {
+    const userRecord = await auth.createUser({
+      displayName: fullName,
+      email,
+      password,
+    });
+    const uid = userRecord.uid;
 
-  const uid = userRecord.uid;
+    await auth.setCustomUserClaims(uid, {
+      accountType: "patient",
+    });
 
-  await auth.setCustomUserClaims(uid, {
-    accountType: "patient",
-  });
+    await db.collection("users").doc(uid).set({
+      accountType: "patient",
+    });
 
-  await db.collection("users").doc(uid).set({
-    accountType: "patient",
-  });
+    const docSnap = await db.collection("users").doc(uid).get();
 
-  const docSnap = await db.collection("users").doc(uid).get();
+    if (!docSnap.exists) {
+      throw new HttpError("Could not retrieve created user");
+    }
 
-  if (!docSnap.exists) {
-    throw new HttpError("Could not retrieve created user");
+    return {
+      uid,
+      ...docSnap.data(),
+    };
+  } catch (e) {
+    switch (e.code) {
+      case "auth/email-already-exists":
+        throw new HttpError("Email already exists", 400);
+    }
   }
-
-  return {
-    uid,
-    ...docSnap.data(),
-  };
 };
