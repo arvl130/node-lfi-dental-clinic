96df28e8406130b2c51ac01042e837f4680f3511
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-16 17:36:40 +0800

    newAppointment.js: send confirmation email after cancelling appointment

--- helpers/appointments/cancelAppointment.js
+++ helpers/appointments/cancelAppointment.js
@@ -1,7 +1,10 @@
 const { getFirestore, FieldValue } = require("firebase-admin/firestore");
+const { getAuth } = require("firebase-admin/auth");
 const getMonthSecondsFromSlotSeconds = require("../conversions/getMonthSecondsFromSlotSeconds");
 const HttpError = require("../HttpError");
+const sendEmail = require("./sendEmail");
 const db = getFirestore();
+const auth = getAuth();
 
 module.exports = async (patientUid, slotSeconds) => {
   const monthSeconds = getMonthSecondsFromSlotSeconds(parseInt(slotSeconds));
@@ -35,4 +38,41 @@ module.exports = async (patientUid, slotSeconds) => {
   });
 
   await batch.commit();
+
+  const userRecord = await auth.getUser(patientUid);
+  const patientName = userRecord.displayName;
+  const email = userRecord.email;
+  const formattedDate = new Date(slotSeconds * 1000).toLocaleString("en-us", {
+    timeZone: "Asia/Manila",
+    month: "long",
+    year: "numeric",
+    day: "numeric",
+  });
+  const hours = new Date(slotSeconds * 1000)
+    .toLocaleString("en-us", {
+      timeZone: "Asia/Manila",
+    })
+    .split(" ")[1]
+    .split(":")[0];
+  const minutes = new Date(slotSeconds * 1000)
+    .toLocaleString("en-us", {
+      timeZone: "Asia/Manila",
+    })
+    .split(" ")[1]
+    .split(":")[1];
+  const ampm = new Date(slotSeconds * 1000)
+    .toLocaleString("en-us", {
+      timeZone: "Asia/Manila",
+    })
+    .split(" ")[2];
+  await sendEmail(
+    email,
+    "LFI Dental Clinic - Your appointment has been cancelled",
+    `
+The following appointment has been cancelled:
+Date and time: ${formattedDate} ${hours}:${minutes} ${ampm}
+Patient name: ${patientName}
+Service: ${userAppointmentSnapshot.data().service ?? "N/A"}
+  `
+  );
 };
