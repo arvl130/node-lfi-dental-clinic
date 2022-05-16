fdc57d30f0c330f84d0ffa8c292b6ce28f4d1cc3
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-16 17:35:34 +0800

    newAppointment.js: send confirmation email after setting new appointment

--- helpers/appointments/newAppointment.js
+++ helpers/appointments/newAppointment.js
@@ -1,6 +1,9 @@
 const { getFirestore, FieldValue } = require("firebase-admin/firestore");
+const { getAuth } = require("firebase-admin/auth");
 const getMonthSecondsFromSlotSeconds = require("../conversions/getMonthSecondsFromSlotSeconds");
+const sendEmail = require("./sendEmail");
 const db = getFirestore();
+const auth = getAuth();
 
 module.exports = async (patientUid, slotSeconds, service) => {
   const monthSeconds = getMonthSecondsFromSlotSeconds(parseInt(slotSeconds));
@@ -41,4 +44,41 @@ module.exports = async (patientUid, slotSeconds, service) => {
   );
 
   await batch.commit();
+
+  const userRecord = await auth.getUser(patientUid);
+  const patientName = userRecord.displayName;
+  const email = userRecord.email;
+  const formattedDate = new Date(slotSeconds * 1000).toLocaleString("en-us", {
+    timeZone: "Asia/Manila",
+    month: "long",
+    year: "numeric",
+    day: "numeric",
+  });
+  const hours = new Date(slotSeconds * 1000)
+    .toLocaleString("en-us", {
+      timeZone: "Asia/Manila",
+    })
+    .split(" ")[1]
+    .split(":")[0];
+  const minutes = new Date(slotSeconds * 1000)
+    .toLocaleString("en-us", {
+      timeZone: "Asia/Manila",
+    })
+    .split(" ")[1]
+    .split(":")[1];
+  const ampm = new Date(slotSeconds * 1000)
+    .toLocaleString("en-us", {
+      timeZone: "Asia/Manila",
+    })
+    .split(" ")[2];
+  await sendEmail(
+    email,
+    "LFI Dental Clinic - You have been reserved an appointment",
+    `
+The following appointment has been reserved:
+Date and time: ${formattedDate} ${hours}:${minutes} ${ampm}
+Patient name: ${patientName}
+Service: ${service}
+  `
+  );
 };
