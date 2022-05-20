8e398838be1a7356e9e848d64e955faf475b634b
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-20 23:51:46 +0800

    helpers/getMedicalChart.js: fallback to default entries, if there is no medical chart found

--- helpers/users/medical-chart/getMedicalChart.js
+++ helpers/users/medical-chart/getMedicalChart.js
@@ -1,6 +1,5 @@
-const { getFirestore } = require("firebase-admin/firestore");
-const { HttpError } = require("../../HttpError");
-const db = getFirestore();
+const { getFirestore } = require("firebase-admin/firestore")
+const db = getFirestore()
 
 module.exports = async (patientUid) => {
   const docSnap = await db
@@ -8,9 +7,49 @@ module.exports = async (patientUid) => {
     .doc(patientUid)
     .collection("patientRecords")
     .doc("medicalChart")
-    .get();
+    .get()
 
-  if (!docSnap.exists) throw new HttpError("No such document", 404);
-
-  return docSnap.data();
-};
+  if (docSnap.exists) return docSnap.data()
+  else
+    return {
+      personalInformation: {
+        fullName: "",
+        gender: "",
+        birthDate: "",
+        maritalStatus: "",
+        mobileNo: "",
+        telephoneNo: "",
+      },
+      medicalHistory: {
+        heartAilmentDisease: "",
+        hospitalAdmission: "",
+        selfMedication: "",
+        allergies: "",
+        operation: "",
+        tumors: "",
+        diabetes: false,
+        sinusitis: false,
+        bleedingGums: false,
+        hypertension: false,
+        stomachDisease: false,
+        bloodDisease: false,
+        headache: false,
+        liverDisease: false,
+        cold: false,
+        kidneyDisease: false,
+        pregnant: "",
+        familyHistory: "",
+      },
+      dentalHistory: {
+        bleedingInMouth: false,
+        gumsChangeColor: false,
+        sensitiveTeeth: false,
+        badBreath: false,
+        palate: false,
+        teethChangeColor: false,
+        lumpsInMouth: false,
+        clickingSoundInMouth: false,
+        pastDentalCare: "",
+      },
+    }
+}
