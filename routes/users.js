be3a4ce10f0177df5c42788b66973551d69be4cf
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-20 22:34:34 +0800

    users.js: use new appointment states: pending, attended, not attended

--- routes/users.js
+++ routes/users.js
@@ -1,83 +1,90 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 
-const updateMedicalChart = require("../controllers/users/updateMedicalChart");
-const getUserProfile = require("../controllers/users/getUserProfile");
-const listUsers = require("../controllers/users/listUsers");
-const requirePatientToken = require("../middleware/requirePatientToken");
-const requirePatientOrAdminToken = require("../middleware/requirePatientOrAdminToken");
-const requireAdminToken = require("../middleware/requireAdminToken");
-const getUserAppointments = require("../controllers/users/getUserAppointments");
-const setUserAppointmentAttended = require("../controllers/users/setUserAppointmentAttended");
-const unsetUserAppointmentAttended = require("../controllers/users/unsetUserAppointmentAttended");
-const getUserAppointmentProcedure = require("../controllers/users/getUserAppointmentProcedure");
-const setUserAppointmentProcedure = require("../controllers/users/setUserAppointmentProcedure");
-const getMedicalChart = require("../controllers/users/medical-chart/getMedicalChart");
+const updateMedicalChart = require("../controllers/users/updateMedicalChart")
+const getUserProfile = require("../controllers/users/getUserProfile")
+const listUsers = require("../controllers/users/listUsers")
+const requirePatientToken = require("../middleware/requirePatientToken")
+const requirePatientOrAdminToken = require("../middleware/requirePatientOrAdminToken")
+const requireAdminToken = require("../middleware/requireAdminToken")
+const getUserAppointments = require("../controllers/users/getUserAppointments")
+const setUserAppointmentAttended = require("../controllers/users/setUserAppointmentAttended")
+const setUserAppointmentNotAttended = require("../controllers/users/setUserAppointmentNotAttended")
+const setUserAppointmentPending = require("../controllers/users/setUserAppointmentPending")
+const getUserAppointmentProcedure = require("../controllers/users/getUserAppointmentProcedure")
+const setUserAppointmentProcedure = require("../controllers/users/setUserAppointmentProcedure")
+const getMedicalChart = require("../controllers/users/medical-chart/getMedicalChart")
 
-router.get("/", requireAdminToken, listUsers);
+router.get("/", requireAdminToken, listUsers)
 
-router.get("/:patientUid", requirePatientOrAdminToken, getUserProfile);
+router.get("/:patientUid", requirePatientOrAdminToken, getUserProfile)
 
 router.get(
   "/:patientUid/appointments",
   requirePatientOrAdminToken,
   getUserAppointments
-);
+)
+
+router.put(
+  "/:patientUid/appointments/:slotSeconds/attended",
+  requireAdminToken,
+  setUserAppointmentAttended
+)
 
 router.patch(
   "/:patientUid/appointments/:slotSeconds/attended",
   requireAdminToken,
-  setUserAppointmentAttended
-);
+  setUserAppointmentNotAttended
+)
 
 router.delete(
   "/:patientUid/appointments/:slotSeconds/attended",
   requireAdminToken,
-  unsetUserAppointmentAttended
-);
+  setUserAppointmentPending
+)
 
 router.get(
   "/:patientUid/appointments/:slotSeconds/procedure",
   requireAdminToken,
   getUserAppointmentProcedure
-);
+)
 
 router.patch(
   "/:patientUid/appointments/:slotSeconds/procedure",
   requireAdminToken,
   setUserAppointmentProcedure
-);
+)
 
 router.patch(
   "/:patientUid/medicalchart",
   requirePatientOrAdminToken,
   updateMedicalChart
-);
+)
 
 router.get(
   "/:patientUid/medicalchart",
   requirePatientOrAdminToken,
   getMedicalChart
-);
+)
 
 router.patch(
   "/:patientUid/dentalchart",
   requirePatientToken,
   updateMedicalChart
-);
+)
 
-router.get("/:patientUid/dentalchart", requirePatientToken, updateMedicalChart);
+router.get("/:patientUid/dentalchart", requirePatientToken, updateMedicalChart)
 
 router.delete(
   "/:patientUid/appointments/:slotSeconds/procedure",
   requirePatientToken,
   updateMedicalChart
-);
+)
 
 router.delete(
   "/:patientUid/appointments/:slotSeconds/cancel",
   requirePatientToken,
   updateMedicalChart
-);
+)
 
-module.exports = router;
+module.exports = router
