606518c803f4991497a963a916a64b562a25a257
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-20 22:31:50 +0800

    helpers/setUserAppointmentNotAttended.js: add helper

---
+++ helpers/users/setUserAppointmentNotAttended.js
@@ -0,0 +1,18 @@
+const { getFirestore } = require("firebase-admin/firestore")
+
+const db = getFirestore()
+module.exports = async (patientUid, slotSeconds) => {
+  await db
+    .collection("users")
+    .doc(patientUid)
+    .collection("appointments")
+    .doc(slotSeconds)
+    .set(
+      {
+        attended: false,
+      },
+      {
+        merge: true,
+      }
+    )
+}
