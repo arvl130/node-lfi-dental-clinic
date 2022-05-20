0cb1fb975f0a89534595f6c5098b906d51d2050d
Angelo Geulin <angelogeulin123@gmail.com>
2022-05-21 03:38:23 +0800

    helpers/registerPatientUser.js: add field for telling if medical chart is filled up already

--- helpers/registerPatientUser.js
+++ helpers/registerPatientUser.js
@@ -1,8 +1,8 @@
-const { getFirestore } = require("firebase-admin/firestore");
-const db = getFirestore();
-const { getAuth } = require("firebase-admin/auth");
-const auth = getAuth();
-const HttpError = require("./HttpError");
+const { getFirestore } = require("firebase-admin/firestore")
+const db = getFirestore()
+const { getAuth } = require("firebase-admin/auth")
+const auth = getAuth()
+const HttpError = require("./HttpError")
 
 module.exports = async (email, password, fullName) => {
   try {
@@ -10,31 +10,32 @@ module.exports = async (email, password, fullName) => {
       displayName: fullName,
       email,
       password,
-    });
-    const uid = userRecord.uid;
+    })
+    const uid = userRecord.uid
 
     await auth.setCustomUserClaims(uid, {
       accountType: "patient",
-    });
+    })
 
     await db.collection("users").doc(uid).set({
       accountType: "patient",
-    });
+      filledInMedicalChart: false,
+    })
 
-    const docSnap = await db.collection("users").doc(uid).get();
+    const docSnap = await db.collection("users").doc(uid).get()
 
     if (!docSnap.exists) {
-      throw new HttpError("Could not retrieve created user");
+      throw new HttpError("Could not retrieve created user")
     }
 
     return {
       uid,
       ...docSnap.data(),
-    };
+    }
   } catch (e) {
     switch (e.code) {
       case "auth/email-already-exists":
-        throw new HttpError("Email already exists", 400);
+        throw new HttpError("Email already exists", 400)
     }
   }
-};
+}
