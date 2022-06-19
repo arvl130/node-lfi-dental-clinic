855be9fb46fed33d9c8af82edaaa276e32aa67e6
Angelo Geulin <angelogeulin123@gmail.com>
2022-06-19 11:08:45 +0800

    index.js: remove semicolons

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const port = process.env.PORT || 5000;
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const port = process.env.PORT || 5000
 
 // register middleware for CORS and JSON
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // initialize Firebase Admin
-require("./firebase");
+require("./firebase")
 
-const authRoute = require("./routes/auth");
-const messagesRoute = require("./routes/messages");
-const timeslotsRoute = require("./routes/timeslots");
-const usersRoute = require("./routes/users");
-const appointmentsRoute = require("./routes/appointments");
+const authRoute = require("./routes/auth")
+const messagesRoute = require("./routes/messages")
+const timeslotsRoute = require("./routes/timeslots")
+const usersRoute = require("./routes/users")
+const appointmentsRoute = require("./routes/appointments")
 
-app.use("/users", usersRoute);
-app.use("/auth", authRoute);
-app.use("/messages", messagesRoute);
-app.use("/timeslots", timeslotsRoute);
-app.use("/appointments", appointmentsRoute);
+app.use("/users", usersRoute)
+app.use("/auth", authRoute)
+app.use("/messages", messagesRoute)
+app.use("/timeslots", timeslotsRoute)
+app.use("/appointments", appointmentsRoute)
 
 app.use((req, res) => {
   res.status(404).json({
     message: "No such endpoint",
-  });
-});
+  })
+})
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port} ...`);
-});
+  console.log(`Listening on port ${port} ...`)
+})
