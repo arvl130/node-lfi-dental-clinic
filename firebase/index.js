8e9d05a95d9633c86756769ffce65075f1533937
Angelo Geulin <angelogeulin123@gmail.com>
2022-06-19 12:05:35 +0800

    firebase: load Firebase Admin credentials from .env variables

--- firebase/index.js
+++ firebase/index.js
@@ -1,6 +1,22 @@
-const admin = require("firebase-admin");
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+const admin = require("firebase-admin")
+
+if (!process.env.FIREBASE_ADMIN_PROJECT_ID)
+  throw new Error("Missing or invalid Firebase Admin project ID")
+
+if (!process.env.FIREBASE_ADMIN_PRIVATE_KEY)
+  throw new Error("Missing or invalid Firebase Admin private key")
+
+if (!process.env.FIREBASE_ADMIN_CLIENT_EMAIL)
+  throw new Error("Missing or invalid Firebase Admin client email")
+
+if (!process.env.FIREBASE_ADMIN_TOKEN_URI)
+  throw new Error("Missing or invalid Firebase Admin token URI")
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+  }),
+})
