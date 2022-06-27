67e1d9305cc9cbef21b605124a2e046dcb069ffb
Angelo Geulin <angelogeulin123@gmail.com>
2022-06-27 14:27:09 +0800

    middleware/requirePatientToken.js: ignore token if running in development environment

--- middleware/requirePatientToken.js
+++ middleware/requirePatientToken.js
@@ -1,30 +1,36 @@
-const { getAuth } = require("firebase-admin/auth");
-const HttpError = require("../helpers/HttpError");
+const { getAuth } = require("firebase-admin/auth")
+const HttpError = require("../helpers/HttpError")
+const isDevelopmentEnvironment = require("../helpers/check-environment/isDevelopmentEnvironment")
 
-const auth = getAuth();
+const auth = getAuth()
 
 module.exports = async (req, res, next) => {
   try {
-    const authorizationHeader = req.headers.authorization;
+    if (isDevelopmentEnvironment()) {
+      next()
+      return
+    }
+
+    const authorizationHeader = req.headers.authorization
 
     if (!authorizationHeader)
-      throw new HttpError("Missing or invalid authorization header", 400);
+      throw new HttpError("Missing or invalid authorization header", 400)
 
-    const idToken = authorizationHeader.split(" ")[1];
+    const idToken = authorizationHeader.split(" ")[1]
 
-    if (!idToken) throw new HttpError("Missing or invalid ID token", 400);
+    if (!idToken) throw new HttpError("Missing or invalid ID token", 400)
 
-    const decodedToken = await auth.verifyIdToken(idToken);
+    const decodedToken = await auth.verifyIdToken(idToken)
 
     if (decodedToken.accountType !== "patient")
-      throw new HttpError("Unauthorized request", 401);
+      throw new HttpError("Unauthorized request", 401)
 
-    req.patientUid = decodedToken.uid;
+    req.patientUid = decodedToken.uid
 
-    next();
+    next()
   } catch (e) {
     res.status(e.httpErrorCode || 500).json({
       message: `Error occured while getting verifying token: ${e.message}`,
-    });
+    })
   }
-};
+}
