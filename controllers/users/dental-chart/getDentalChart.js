3a67eee8547da3f2c1251a4ad41bc3d141fd285e
Angelo Geulin <angelogeulin123@gmail.com>
2022-06-27 21:34:07 +0800

    controllers/getDentalChart.js: add controller

---
+++ controllers/users/dental-chart/getDentalChart.js
@@ -0,0 +1,26 @@
+const getDentalChart = require("../../../helpers/users/dental-chart/getDentalChart")
+const HttpError = require("../../../helpers/HttpError")
+const { getAuth } = require("firebase-admin/auth")
+const auth = getAuth()
+
+module.exports = async (req, res) => {
+  try {
+    const patientUid = req.params.patientUid
+
+    if (!patientUid) throw new HttpError("Missing or invalid patient UID", 400)
+
+    const dentalChart = await getDentalChart(patientUid)
+
+    res.status(200).json({
+      message: "Updated dental chart",
+      payload: {
+        uid: patientUid,
+        ...dentalChart,
+      },
+    })
+  } catch (e) {
+    res.status(e.httpErrorCode || 500).json({
+      message: `Error occured while getting dental chart: ${e.message}`,
+    })
+  }
+}
