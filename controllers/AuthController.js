3f487fbc12e4d2cb63bb167843b853a019020f99
Angelo Geulin <angelogeulin123@gmail.com>
2022-10-08 20:48:40 +0800

    controllers/AuthController.js: add controller

---
+++ controllers/AuthController.js
@@ -0,0 +1,72 @@
+const HttpError = require("../helpers/HttpError")
+const { createAdmin, createPatient } = require("../models/Users")
+
+async function registerAdmin(req, res) {
+  try {
+    const email = req.body.email
+    const password = req.body.password
+
+    if (!email) {
+      throw new HttpError("Missing or invalid email", 400)
+    }
+
+    if (!password) {
+      throw new HttpError("Missing or invalid password", 400)
+    }
+
+    if (password && password.length < 8) {
+      throw new HttpError("Password too short", 400)
+    }
+
+    const user = await createAdmin(email, password)
+
+    res.status(200).json({
+      message: "User created",
+      payload: user,
+    })
+  } catch (e) {
+    res.status(e.httpErrorCode || 500).json({
+      message: `Error occured while creating user: ${e.message}`,
+    })
+  }
+}
+
+async function registerPatient(req, res) {
+  try {
+    const email = req.body.email
+    const password = req.body.password
+    const fullName = req.body.fullName
+
+    if (!email) {
+      throw new HttpError("Missing or invalid email", 400)
+    }
+
+    if (!password) {
+      throw new HttpError("Missing or invalid password", 400)
+    }
+
+    if (password && password.length < 8) {
+      throw new HttpError("Password too short", 400)
+    }
+
+    if (!fullName) {
+      throw new HttpError("Missing or invalid full name", 400)
+    }
+
+    const user = await createPatient(email, password, fullName)
+
+    res.status(200).json({
+      message: "User created",
+      payload: user,
+    })
+  } catch (e) {
+    res.status(e.httpErrorCode || 500).json({
+      message: `Error occured while creating user: ${e.message}`,
+    })
+  }
+}
+
+module.exports = {
+  registerAdmin,
+  registerPatient,
+}
